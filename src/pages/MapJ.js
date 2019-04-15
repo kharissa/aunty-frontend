e68d7310cc6f7730d463a0745117637ba2a39062
jnsy <jadensy99@gmail.com>
2019-04-15 12:29:48 +0800

    populate markers from list

--- src/pages/MapJ.js
+++ src/pages/MapJ.js
@@ -8,11 +8,16 @@ class MapJ extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            mapCenter: [3.134526, 101.630016],
+            mapCenter: [3.134526, 101.63006],
             mapZoom: 15,
             mapTilesStamen: 'http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
             mapOverlayStamen: 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.png',
-            marker: [3.134526, 101.630016]
+            marker: [3.134526, 101.530016],
+            markers: [
+                { id: "1", lat: 3.134526, lng: 101.630016, name: "Test Marker 01" },
+                { id: "2", lat: 3.154526, lng: 101.730016, name: "Test Marker 02" },
+            ],
+            clickedMarker : [],
         }
     }
 
@@ -23,7 +28,9 @@ class MapJ extends React.Component {
 
     handleClick = (e) => {
         const { lat, lng } = e.latlng;
-        console.log(lat, lng);
+        // console.log(lat, lng);
+        this.setState({ clickedMarker: [lat, lng] })
+        console.log(this.state.clickedMarker)
     }
 
     render() {
@@ -40,22 +47,31 @@ class MapJ extends React.Component {
                 animate={true}
                 easeLinearity={0.35}
                 onclick={this.handleClick}
+                // url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             >
-                <TileLayer
-                    url={this.state.mapTilesStamen}
-                    // url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
-                <TileLayer
-                    url={this.state.mapOverlayStamen}
-                />
+
+                <TileLayer url={this.state.mapTilesStamen} />
+                <TileLayer url={this.state.mapOverlayStamen} />
+
                 <Marker position={this.state.marker}>
                     <Popup>
-                        Popup for any custom information.
-          </Popup>
+                        <p>Hardcoded Marker</p>
+                        {this.state.marker.join(", ")}
+                    </Popup>
                 </Marker>
+
+                {this.state.markers.map((marker, index) =>
+                    <Marker key={index} position={[marker.lat, marker.lng]}>
+                        <Popup>
+                            <p>{marker.name}</p>
+                        </Popup>
+                    </Marker>
+                )}
+
             </LeafletMap>
         );
     }
 }
 
 export default MapJ;
+
