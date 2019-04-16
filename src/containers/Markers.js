4650f09e7a558ec5e5a57618e5631e604d1f01e0
jnsy <jadensy99@gmail.com>
2019-04-16 15:31:56 +0800

    add changing icons based on pin categories

--- src/containers/Markers.js
+++ src/containers/Markers.js
@@ -23,3 +23,4 @@
 //         <LeafletMap></LeafletMap>
 //     )
 // }
+
--- src/pages/MapJ.js
+++ src/pages/MapJ.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { renderToStaticMarkup } from "react-dom/server";
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import CreatePinModal from '../containers/CreatePinModal';
+import { divIcon } from 'leaflet';
 
 
 class MapJ extends React.Component {
@@ -12,11 +13,13 @@ class MapJ extends React.Component {
             mapCenter: [3.134526, 101.63006],
             mapZoom: 15,
             mapTilesStamen: 'http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
+            mapTilesCarto: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
             mapOverlayStamen: 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.png',
+            mapTilesMapTiler: 'https://api.maptiler.com/maps/positron/{z}/{x}/{y}.png?key=QQTisrA3O6303lHbHZ4u',
             marker: [3.134526, 101.670016],
             markers: [
-                { id: "1", lat: 3.134526, lng: 101.630016, name: "Test Marker 01" },
-                { id: "2", lat: 3.154526, lng: 101.650016, name: "Test Marker 02" },
+                { id: "1", lat: 3.134526, lng: 101.630016, name: "Test Marker 01", category: "Theft" },
+                { id: "2", lat: 3.154526, lng: 101.650016, name: "Test Marker 02", category: "Arson" },
                 // to GET data from API
             ],
             clickedMarker : [],
@@ -37,7 +40,7 @@ class MapJ extends React.Component {
         console.log(this.state.clickedMarker)
     }
 
-    handleMarkerClick = (e) => {s
+    handleMarkerClick = (e) => {
         console.log('clicked on this marker')
     }
 
@@ -48,19 +51,16 @@ class MapJ extends React.Component {
     }
 
     render() {
+        // const iconMarkup = ;
+        const cannaIcon = divIcon({ html: renderToStaticMarkup(<i className=" fa fa-cannabis fa-3x" />) });
+        const theftIcon = divIcon({ html: renderToStaticMarkup(<i className=" fa fa-angry fa-3x" />) });
+        const murderIcon = divIcon({ html: renderToStaticMarkup(<i className=" fa fa-skull fa-3x" />) });
+
         return (
-
-        const iconMarkup = renderToStaticMarkup(
-            <i className=" fa fa-cannabis fa-3x" />
-        );
-        const customMarkerIcon = divIcon({
-                html: iconMarkup
-            });
-
             <LeafletMap
                 center={this.state.mapCenter}
                 zoom={this.state.mapZoom}
-                maxZoom={17}
+                maxZoom={20}
                 attributionControl={true}
                 zoomControl={true}
                 doubleClickZoom={true}
@@ -72,16 +72,25 @@ class MapJ extends React.Component {
                 // url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             >
 
-                <TileLayer url={this.state.mapTilesStamen} />
-                <TileLayer url={this.state.mapOverlayStamen} />
+                <TileLayer url={this.state.mapTilesCarto} />
+                {/* <TileLayer url={this.state.mapOverlayStamen} /> */}
 
-                <Marker position={this.state.marker} icon={customMarkerIcon}>
+                <Marker position={this.state.marker} icon={cannaIcon}>
                     <Popup>
                         <p>Hardcoded Marker</p>
                         {this.state.marker.join(", ")}
                     </Popup>
                 </Marker>
 
+                { this.state.markers.map((marker, index) =>
+                        <Marker key={index} position={[marker.lat, marker.lng]}
+                        icon={marker.category == "Theft" ? theftIcon : murderIcon}
+                        >
+                            <Popup>
+                                <p>{marker.name}</p>
+                            </Popup>
+                        </Marker>
+                    )}
 
                 {this.state.clickedMarker.length > 0
                     ? <Marker className="new-marker" position = {this.state.clickedMarker} onclick={this.toggleModal}></Marker>
