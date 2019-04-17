3af558ab8db79eade1d9f6e5b1b78c33cdd78355
jnsy <jadensy99@gmail.com>
2019-04-17 13:46:38 +0800

    give geolocation a marker

--- src/pages/Geolocation.js
+++ src/pages/Geolocation.js
@@ -1,47 +1,7 @@
-// import React from 'react';
-// import Map from '../containers/Map'
-
-// export default class Geolocation extends React.Component {
-//     state = {
-//         isGeolocationAvailable: '',
-//         isGeolocationEnabled: '',
-//         latitude: 0,
-//         longitude: 0,
-//     }
-//     componentDidMount() {
-//         this.setState({
-//             isGeolocationAvailable: localStorage.getItem('isGeolocationAvailable'),
-//             isGeolocationEnabled: localStorage.getItem('isGeolocationEnabled'),
-//             latitude: localStorage.getItem('latitude'),
-//             longitude: localStorage.getItem('longitude'),
-//         })
-//     }
-//     render() {
-//         if (!this.state.isGeolocationAvailable) {
-//             return (
-//             <div>Your browser does not support Geolocation</div>
-//             )
-//         }
-//         else if (!this.state.isGeolocationEnabled){
-//             return (
-//             <div>Geolocation is not enabled</div>
-//             )
-//         }
-//         else if (this.state.latitude)
-//             return(
-//                 <Map my_lat={this.state.latitude} my_lng={this.state.longitude} />
-//             )
-//         else {
-//             return(
-//                 <div>Getting the location data&hellip;</div>
-//             )
-//         }
-//     }
-// }
-
-
 import React from 'react';
+import { renderToStaticMarkup } from "react-dom/server";
 import { Marker, Popup } from 'react-leaflet';
+import { divIcon } from 'leaflet';
 
 export default class Geolocation extends React.Component {
     state = {
@@ -59,6 +19,8 @@ export default class Geolocation extends React.Component {
         })
     }
     render() {
+        const geolocIcon = divIcon({ html: renderToStaticMarkup(<i className=" fa fa-circle fa-3x" />) });
+
         if (!this.state.isGeolocationAvailable) {
             return (
                 <div>Your browser does not support Geolocation</div>
@@ -73,7 +35,7 @@ export default class Geolocation extends React.Component {
             console.log(this.state.latitude)
             console.log(this.state.longitude);
             return (
-                    <Marker position={[this.state.latitude, this.state.longitude]}>
+                    <Marker position={[this.state.latitude, this.state.longitude]} icon={geolocIcon}>
                         <Popup>Geolocated Position</Popup>
                     </Marker>
             )
